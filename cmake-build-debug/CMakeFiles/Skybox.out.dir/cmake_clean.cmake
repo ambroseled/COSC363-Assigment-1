757f82dbfdaf4ccef6ee7cc8bee2b7ea43acfe62
Ambrose Ledbrook <ajl190@uclive.ac.nz>
2019-03-28 19:36:26 +1300

    Adding lab 4 as template

---
+++ cmake-build-debug/CMakeFiles/Skybox.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Skybox.out.dir/Skybox.cpp.o"
+  "Skybox.out.pdb"
+  "Skybox.out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Skybox.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
